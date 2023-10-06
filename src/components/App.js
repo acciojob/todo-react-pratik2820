621679032bb8f0b060141ec43290d7c07d533233
pratik2820 <pratikmorge28@gmail.com>
2023-10-06 11:27:30 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[inputText,setInputText] = useState("")
+  const[todoList,setTodoList] = useState([])
+ 
+
+  function addItem(){
+    if(inputText !== ""){
+      setTodoList([...todoList,inputText])
+      setInputText(" ")
+    }
+
+  }
+
+  function deleteItem(index){
+    let tempArr = [...todoList]
+    tempArr.splice(index,1)
+    setTodoList([...tempArr])
+
+  }
+
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <h1>ToDo</h1>
-        <p>Hello</p>
+    <div className="mainContainer">
+      <div className="inputContainer">
+        <h1>To-Do List</h1>
+        <input type="text" onChange={(e)=>setInputText(e.target.value)}/>
+        <button onClick={addItem}>Add Todo</button>
+
+        <div className="listContainer">
+          <ul>
+            {
+              todoList.map((item,index)=>(
+                <li key={index} id="index">
+                  <h3>{item}</h3>
+                  <button onClick={deleteItem}>Delete</button>
+                </li>
+
+              ))
+            }
+          </ul>
+
+        </div>
+
+
+      </div>
+        
     </div>
   )
 }
